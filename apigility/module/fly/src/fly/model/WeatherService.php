dd57aab1b5ad7c9973682638a6cab12e86709ba7
Thomas Schwanhaeuser <thomas.schwanhaeuser@solvians.com>
2014-12-14 13:04:27 +0100

    Use static weather

--- apigility/module/fly/src/fly/model/WeatherService.php
+++ apigility/module/fly/src/fly/model/WeatherService.php
@@ -6,31 +6,14 @@ Class WeatherService {
 
     public function getListOfPossibleDestinations($weatherType, $date) {
 
+        $useStaticData = true; // do this for performance reasons;
 
-        //$countries = json_decode(file_get_contents(__DIR__.'/countriestocities.json'));
-
-        /*
-         * Go to a weather service
-         * 
-         * query forecaast for a bunch of cities in one call for that date
-         * 
-         * iterate through the result, filter for matching cities"
-         * 
-         * return array of city codes
-         * 
-         * 
-         * 
-         *  "weather": {
-          "temperatureForecastDepartureDate": 25.7,
-          "magdaFactor": 2,
-          "carlaLiking": 5
-          },
-         */
+        if ($useStaticData) {
+            return $this->getStaticForecasts();
+        }
 
         $airportService = new AirportService();
-
         $knownDestinations = $airportService->getCityAirportMap();
-
 
         $dateObj = \DateTime::createFromFormat("Y-m-d", $date);
 
@@ -42,15 +25,12 @@ Class WeatherService {
             }
         }
 
+        return $results;
+    }
 
-
-
+    public function getStaticForecasts() {
+        $results = array();
         
-
-
-#foreach ($countries as $countryId => $country) { foreach ($country as $cities) { var_dump($countryId, $cities);}}
-/*
-
         $results[] = array(
             'cityName' => 'Dubai',
             'temperatureForecastDepartureDate' => 25.7
@@ -65,7 +45,81 @@ Class WeatherService {
             'temperatureForecastDepartureDate' => 23.1
         );
 
-*/
+        $results[] = array(
+            'cityName' => 'Bangkok',
+            'temperatureForecastDepartureDate' => 29.5
+        );
+
+        $results[] = array(
+            'cityName' => 'Istanbul',
+            'temperatureForecastDepartureDate' => 19.3
+        );
+        $results[] = array(
+            'cityName' => 'Athens',
+            'temperatureForecastDepartureDate' => 15.1
+        );
+        $results[] = array(
+            'cityName' => 'Tel Aviv',
+            'temperatureForecastDepartureDate' => 25.1
+        );
+        $results[] = array(
+            'cityName' => 'Bejing',
+            'temperatureForecastDepartureDate' => 20.1
+        );
+        $results[] = array(
+            'cityName' => 'Barcelona',
+            'temperatureForecastDepartureDate' => 14.8
+        );
+        $results[] = array(
+            'cityName' => 'Lisbon',
+            'temperatureForecastDepartureDate' => 18.9
+        );
+        $results[] = array(
+            'cityName' => 'Cairo',
+            'temperatureForecastDepartureDate' => 21.3
+        );
+        $results[] = array(
+            'cityName' => 'San Fransisco',
+            'temperatureForecastDepartureDate' => 24.3
+        );
+        $results[] = array(
+            'cityName' => 'Lod Angeles',
+            'temperatureForecastDepartureDate' => 28.3
+        );
+        $results[] = array(
+            'cityName' => 'Mexico City',
+            'temperatureForecastDepartureDate' => 31.3
+        );
+        $results[] = array(
+            'cityName' => 'Caracas',
+            'temperatureForecastDepartureDate' => 30.3
+        );
+        $results[] = array(
+            'cityName' => 'Sao Paolo',
+            'temperatureForecastDepartureDate' => 31.1
+        );
+        $results[] = array(
+            'cityName' => 'Santiago',
+            'temperatureForecastDepartureDate' => 34.1
+        );
+
+        $results[] = array(
+            'cityName' => 'Jakarta',
+            'temperatureForecastDepartureDate' => 35.1
+        );
+
+        $results[] = array(
+            'cityName' => 'Tokyo',
+            'temperatureForecastDepartureDate' => 18.8
+        );
+        
+        
+        // get more random temperatures
+        foreach ($results as $key => $result) {
+            $results[$key]['temperatureForecastDepartureDate'] = rand(170,345)/10;
+        }
+
+
         return $results;
     }
 
@@ -77,7 +131,7 @@ Class WeatherService {
             'timeout' => 2
         ));
 
-        $apiUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?mode=json";
+        $apiUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?mode=json&APPID=";
 
         $uri = $apiUrl . '&q=London&cnt=14';
 
@@ -100,7 +154,7 @@ Class WeatherService {
             if ($reqDateF == $forecastDateF) {
                 return array(
                     'cityName' => $city,
-                    'temperatureForecastDepartureDate' => ($forecast['temp']['day']-273.15 + 20)
+                    'temperatureForecastDepartureDate' => ($forecast['temp']['day'] - 273.15 + 20)
                 );
             }
         }
